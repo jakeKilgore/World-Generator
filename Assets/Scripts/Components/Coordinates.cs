ae3fce76c9f29ed13fa4f2703ff5894e7203f1f2
Jacob Kilgore <jake.h.kilgore@gmail.com>
2019-08-12 22:55:03 -0500

    feature: Implemented determining world position from given hex coordinates.

--- Assets/Scripts/Components/Coordinates.cs
+++ Assets/Scripts/Components/Coordinates.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
 
-public struct Coordinates : IComponentData {
+public struct HexCoordinates : IComponentData {
     public int column;
     public int row;
     public int offset;
+
+    private static readonly float WIDTH_MULTIPLE = Mathf.Sqrt(3);
+    private static readonly float HEIGHT_MULTIPLE = 2f;
+    /// <summary>
+    /// Returns the location of the hex in the worldspace.
+    /// </summary>
+    /// <returns></returns>
+    public float2 Position() {
+        float height = HEIGHT_MULTIPLE;
+        float width = WIDTH_MULTIPLE;
+        float posX = width * (column + (row / 2f));
+        float posY = height * 3/4 * row;
+
+        return new float2(posX, posY);
+    }
 }
