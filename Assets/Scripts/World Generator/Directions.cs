cff0cc4facf71629fd23deba372487924a50d0ff
Jacob Kilgore <jake.h.kilgore@gmail.com>
2021-01-21 17:58:20 -0600

    refactor: rewrote Direction class.

--- Assets/Scripts/World Generator/Directions.cs
+++ Assets/Scripts/World Generator/Directions.cs
@@ -1,19 +1,28 @@
 ﻿using Unity.Mathematics;
 
-namespace Assets.Scripts
+namespace Assets.Scripts.WorldGenerator
 {
-    public enum Direction
+    /// <summary>   Collection of the valid directions to move out of a hexagonal tile. </summary>
+    public static class Direction
     {
-        East,
-        NorthEast,
-        NorthWest,
-        SouthWest,
-        SouthEast,
-        West
-    }
+        private static readonly int3 east = new int3(1, 0, -1);
+        private static readonly int3 northEast = new int3(0, 1, -1);
+        private static readonly int3 northWest = new int3(-1, 1, 0);
+        private static readonly int3 west = new int3(0, -1, 1);
+        private static readonly int3 southWest = new int3(1, -1, 0);
+        private static readonly int3 southEast = new int3(-1, 0, 1);
 
-    public static class Directions
-    {
-        public static readonly int3[] direction = { new int3(1, 0, -1), new int3(0, 1, -1), new int3(-1, 1, 0), new int3(0, -1, 1), new int3(1, -1, 0), new int3(-1, 0, 1) };
+        /// <summary> Vector pointing east from the hex. </summary>
+        public static ref readonly int3 East => ref east;
+        /// <summary> Vector pointing northeast from the hex. </summary>
+        public static ref readonly int3 NorthEast => ref northEast;
+        /// <summary> Vector pointing northwest from the hex. </summary>
+        public static ref readonly int3 NorthWest => ref northWest;
+        /// <summary> Vector pointing west from the hex. </summary>
+        public static ref readonly int3 West => ref west;
+        /// <summary> Vector pointing southwest from the hex. </summary>
+        public static ref readonly int3 SouthWest => ref southWest;
+        /// <summary> Vector pointing southeast from the hex. </summary>
+        public static ref readonly int3 SouthEast => ref southEast;
     }
 }
