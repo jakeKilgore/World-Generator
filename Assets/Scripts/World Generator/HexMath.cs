e40103450dfcd6a8e909e50daafded08354d5558
Jacob Kilgore <jake.h.kilgore@gmail.com>
2021-01-21 22:51:01 -0600

    feat: made hex math moddable by hex radius

--- Assets/Scripts/World Generator/HexMath.cs
+++ Assets/Scripts/World Generator/HexMath.cs
@@ -1,33 +1,30 @@
-﻿////////////////////////////////////////////////////////////////////////////////////////////////////
-// file:	Assets\Scripts\HexMath.cs
-//
-// summary:	Implements math functions for the world generator's implementation of hexagonal tiles.
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-using Unity.Mathematics;
+﻿using Unity.Mathematics;
 
 namespace Assets.Scripts.WorldGenerator {
 
     /// <summary>   Mathematics for the hexagonal tile implementation. </summary>
     public static class HexMath
     {
+        private static readonly float widthMultiple = math.sqrt(3f);
+        private static readonly float heightMultiple = 2f;
 
         /// <summary>   The width multiple of a hexagon. </summary>
         /// <remarks>   Multiply the radius of a pointy-top hexagon by this number to get the width from side to side.</remarks>
-        public static readonly float WidthMultiple = math.sqrt(3f);
+        public static float WidthMultiple => widthMultiple;
         /// <summary>   The height multiple of a hexagon. </summary>
         /// <remarks>   Multiply the radius of a pointy-top hexagon by this number to get the height from point to point.</remarks>
-        public static readonly float HeightMultiple = 2f;
+        public static float HeightMultiple => heightMultiple;
 
         /// <summary>   Returns the location of the hex in the worldspace. </summary>
-        /// <param name="coordinates">   The coordinates of the hex in tile coordinates. </param>
-        /// <returns>   A float2 position in the worldspace. </returns>
-        public static float2 Position(int2 coordinates)
+        /// <param name="coordinates">  The position of the hex in tile coordinates. </param>
+        /// <param name="radius">       The distance from the center of the hexagon to the points. </param>
+        /// <returns>   The position in the worldspace coordinates. </returns>
+        public static float2 Position(int2 coordinates, float radius = 1f)
         {
-            float height = HeightMultiple;
-            float width = WidthMultiple;
-            float posX = WidthMultiple * (coordinates.x + (coordinates.y / 2f));
-            float posY = HeightMultiple * 3 / 4 * coordinates.y;
+            var height = HeightMultiple * radius;
+            var width = WidthMultiple * radius;
+            var posX = WidthMultiple * (coordinates.x + (coordinates.y / 2f));
+            var posY = HeightMultiple * .75f * coordinates.y;
 
             return new float2(posX, posY);
         }
@@ -70,9 +67,9 @@ namespace Assets.Scripts.WorldGenerator {
         public static int MinResolutionOfVertex(int vertexIndex)
         {
             if (vertexIndex == 0) { return 0; }
-            int a = 3;
-            int b = 3;
-            int c = 1 - vertexIndex;
+            var a = 3;
+            var b = 3;
+            var c = 1 - vertexIndex;
             int resolution = (int)(-b + math.sqrt(b * b - (4 * a * c))) / (2 * a);
             return resolution + 1;
         }
