31162a1c937708d6e36941beeaeeaf32689769dd
Jacob Kilgore <jake.h.kilgore@gmail.com>
2021-01-22 16:48:29 -0600

    refactor: changed direction iterator to reference

--- Assets/Scripts/World Generator/Directions.cs
+++ Assets/Scripts/World Generator/Directions.cs
@@ -11,20 +11,27 @@ namespace Assets.Scripts.WorldGenerator
         private static readonly int3 west = new int3(0, -1, 1);
         private static readonly int3 southWest = new int3(1, -1, 0);
         private static readonly int3 southEast = new int3(-1, 0, 1);
+        private static readonly int3[] directions = {East, NorthEast, NorthWest, West, SouthWest, SouthEast};
 
         /// <summary> Vector pointing east from the hex. </summary>
         public static ref readonly int3 East => ref east;
+
         /// <summary> Vector pointing northeast from the hex. </summary>
         public static ref readonly int3 NorthEast => ref northEast;
+
         /// <summary> Vector pointing northwest from the hex. </summary>
         public static ref readonly int3 NorthWest => ref northWest;
+
         /// <summary> Vector pointing west from the hex. </summary>
         public static ref readonly int3 West => ref west;
+
         /// <summary> Vector pointing southwest from the hex. </summary>
         public static ref readonly int3 SouthWest => ref southWest;
+
         /// <summary> Vector pointing southeast from the hex. </summary>
         public static ref readonly int3 SouthEast => ref southEast;
 
-        public static int3[] Directions = {East, NorthEast, NorthWest, West, SouthWest, SouthEast};
+        /// <summary> Iterable list of valid directions. </summary>
+        public static ref readonly int3[] Directions => ref directions;
     }
 }
